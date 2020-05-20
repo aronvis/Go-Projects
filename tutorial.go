4b63b9744dca512178647cb87686d620673c9dc9
Aron Vischjager <vischjag@usc.edu>
2020-05-20 14:38:12 -0700

    Start of basic go tutorial

---
+++ tutorial.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Variable Declaration
+var x int = 5
+
+// Basic Variable Types
+
+// Special Variable Types
+
+// Type Casting
+
+// Functions
+
+// Classes
+
+
+
+
+func main() {
+	fmt.Println(x)
+
+}
